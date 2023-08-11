2f656463fe12b4a8339f6d4fd5cf57e73eba1863
Mo Tausif <mtansari200@gmail.com>
2023-08-11 17:20:11 +0530

    Carousel

--- front-end/src/pages/home.js
+++ front-end/src/pages/home.js
@@ -1,13 +1,14 @@
 import Nav from '../components/navbar';
 import ProductList from '../components/product-list';
 import { useSelector } from 'react-redux';
-
+import Carousel from '../components/carousel';
 
 const Home=()=> {
   const products= useSelector(state=>state.product.products)
   return (
     <>
         <Nav cartCount={3}></Nav>
+        <Carousel></Carousel>
       <ProductList products={products}></ProductList>
     </>
   );
