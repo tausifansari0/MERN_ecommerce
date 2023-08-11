c02ddf445c929c55258340eb4a9083a13eac692b
Mo Tausif <mtansari200@gmail.com>
2023-08-11 17:27:33 +0530

    Footer

--- front-end/src/pages/home.js
+++ front-end/src/pages/home.js
@@ -2,14 +2,15 @@ import Nav from '../components/navbar';
 import ProductList from '../components/product-list';
 import { useSelector } from 'react-redux';
 import Carousel from '../components/carousel';
-
+import Footer from '../components/footer';
 const Home=()=> {
   const products= useSelector(state=>state.product.products)
   return (
     <>
         <Nav cartCount={3}></Nav>
         <Carousel></Carousel>
-      <ProductList products={products}></ProductList>
+        <ProductList products={products}></ProductList>
+        <Footer></Footer>
     </>
   );
 }
